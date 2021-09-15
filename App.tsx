ac5e4f927588476fec59763a3b64b915d12d52a7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-09-15 16:52:59 +0700

    :pencil: add toolbar

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { LogBox } from 'react-native';
 
 import Splash from './src/screens/Splash';
 import ListPlace from './src/screens/ListPlace';
+import { THEME_COLOR } from './src/data/Colors';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +17,16 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
-        <Stack.Screen name="ListPlace" component={ListPlace} />
+        <Stack.Screen name="ListPlace" component={ListPlace} options={{
+          title: 'List Place',
+          headerStyle: {
+            backgroundColor: THEME_COLOR
+          },
+          headerTintColor: '#FFF',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          }
+        }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
